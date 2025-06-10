d251e65ddb204a2dac4234342248e5eefbd9afc3
Joao Pedro <jp.bruno.machado@gmail.com>
2025-06-10 11:33:28 -0300

    refac

--- src/components/BaseModal.tsx
+++ src/components/BaseModal.tsx
@@ -4,8 +4,10 @@ import type { ReactNode } from "react";
 export default function BaseModal({ isOpen, close, title, children }: { isOpen: boolean; close: () => void; title: string; children: ReactNode }) {
     return (
         <Modal open={isOpen} onClose={close} sx={{ display: "grid", placeItems: "center" }}>
-            <Paper>
-                <Typography textAlign="center" sx={{p: 1}}>{title}</Typography>
+            <Paper sx={{maxWidth: "700px"}}>
+                <Typography textAlign="center" sx={{ p: 1 }}>
+                    {title}
+                </Typography>
                 <Divider />
                 <Box sx={{ p: 2 }}>{children}</Box>
             </Paper>
