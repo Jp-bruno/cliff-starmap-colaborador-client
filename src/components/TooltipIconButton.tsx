3398647e5a8263ce42943408db4d5154e4748b95
Joao Pedro <jp.bruno.machado@gmail.com>
2025-06-11 22:47:05 -0300

    disabled state for tooltip icon button

--- src/components/TooltipIconButton.tsx
+++ src/components/TooltipIconButton.tsx
@@ -6,15 +6,17 @@ export default function TooltipIconButton({
     action,
     icon,
     loading,
+    disabled = false
 }: {
     title: string;
     action: (() => void) | ((event: React.MouseEvent<HTMLButtonElement>) => void);
     icon: ReactNode;
     loading?: boolean;
+    disabled?: boolean
 }) {
     return (
         <Tooltip title={title}>
-            <IconButton loading={loading} onClick={action}>
+            <IconButton loading={loading} onClick={action} disabled={disabled}>
                 {icon}
             </IconButton>
         </Tooltip>
