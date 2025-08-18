e0758d298b934fb8d1b19c4d111bb60754fc1f7c
Joao Pedro <jp.bruno.machado@gmail.com>
2025-08-18 16:51:37 -0300

    last changes to multiple file upload

--- src/components/LinearProgressBar.tsx
+++ src/components/LinearProgressBar.tsx
@@ -2,15 +2,15 @@ import { Box, LinearProgress, Typography } from "@mui/material";
 
 export default function LinearProgressBar({ progress, showPercentage }: { progress: number; showPercentage?: boolean }) {
     return (
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <Box sx={{ width: "100%" }}>
-                <LinearProgress variant="determinate" value={progress} />
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", width: "100%" }}>
+            <Box sx={{ width: "100%", display: "flex", alignItems: "center", gap: 1 }}>
+                <LinearProgress variant="determinate" value={progress} sx={{ width: "100%" }} />
+                {showPercentage && (
+                    <Box>
+                        <Typography variant="body2" sx={{ color: "text.secondary" }}>{`${Math.round(progress)}%`}</Typography>
+                    </Box>
+                )}
             </Box>
-            {showPercentage && (
-                <Box>
-                    <Typography variant="body2" sx={{ color: "text.secondary" }}>{`${Math.round(progress)}%`}</Typography>
-                </Box>
-            )}
         </Box>
     );
 }
